0ee0ad08764f8715e04392425fc066e014006a20
Oscar Lindo <77596284+oscar2697@users.noreply.github.com>
2023-09-16 15:26:40 -0500

    Primer Commit

---
+++ src/assets/index.js
@@ -0,0 +1,62 @@
+import logo from "./logo.svg";
+import frontend from "./frontend.png";
+import student from "./student.png";
+import react from "./react.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import java from './tech/java.png';
+// import mongodb from "./tech/mongodb.png";
+import mysql from './tech/mysql.png';
+// import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+//import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+//import typescript from "./tech/typescript.png";
+
+import b1soft from "./company/b1soft.png";
+// import shopify from "./company/shopify.png";
+// import starbucks from "./company/starbucks.png";
+// import tesla from "./company/tesla.png";
+
+import game from "./game.png";
+import gif from "./gif.png";
+import manager from "./manager.png";
+import oscar from './oscar.png';
+
+export {
+  logo,
+  frontend,
+  student,
+  react,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  git,
+  html,
+  javascript,
+  java,
+  //mongodb,
+  mysql,
+  //nodejs,
+  reactjs,
+  //redux,
+  tailwind,
+  //typescript,
+  b1soft,
+  //shopify,
+  game,
+  gif,
+  manager,
+  oscar
+};
---
+++ src/components/Works.jsx
@@ -0,0 +1,69 @@
+import { Tilt } from "react-tilt"
+import { motion } from "framer-motion"
+import { styles } from "../styles"
+import { github } from "../assets"
+import { SectionWrapper } from "../hoc"
+import { projects } from "../constants"
+import { fadeIn, textVariant } from "../utils/motion"
+
+const ProjectCard = ({index, name, description, tags, image, source_code_link}) => {
+  return(
+    <motion.div variants={fadeIn('up', 'spring', index * 0.5, 0.75)}>
+      <Tilt options={{max:45, scale: 1, speed: 450}} className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'>
+        <div className="relative w-full h-[230px] ">
+          <img src={image} alt={name} className="w-full h-full object-cover rounded-2xl"/>
+
+          <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+            <div onClick={() => window.open(source_code_link, 'blank')} 
+              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer">
+                <img src={github} alt="github" className="w-1/2 h-1/2 object-contain"/>
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-5">
+          <h3 className="text-white font-bold text-[24px] ">{name}</h3>
+          <p className="mt-2 text-secondary text-[14px] ">{description} </p>
+        </div>
+
+        <div className="mt-4 flex flex-wrap gap-2">
+          {tags.map((tag) => (
+            <p key={tag.name} className={`text-[14px] ${tag.color}`}>
+              #{tag.name}
+            </p>
+          ))}
+        </div>
+      </Tilt>
+    </motion.div>
+  )
+}
+
+const Works = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>My Work</p>
+        <h2 className={styles.sectionHeadText}>Projects.</h2>
+      </motion.div>
+
+      <div className="w-full flex">
+        <motion.p variants={fadeIn('', '', 0.1, 1)} className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px] ">
+        Through a series of personal projects, I have explored the exciting world of website and app creation. 
+        From my first forays into HTML and CSS to more recent projects with React.js, each job represents a step 
+        in my learning and development. My goal is to showcase my ability to create attractive and functional user interfaces 
+        while continuing to explore and improve my skills in Front-End development. Through these projects, 
+        I hope to convey my passion for technology and my dedication to contributing to the ever-evolving web world. 
+        Join me on my journey to discover how I have brought my ideas and learnings to life in the digital world!
+        </motion.p>
+      </div>
+
+      <div className="mt-20 flex flex-wrap gap-7">
+        {projects.map((project, index) => (
+          <ProjectCard key={`project-${index}`} index={index} {...project} />
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default SectionWrapper(Works, '')
